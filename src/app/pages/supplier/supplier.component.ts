1545fac1633f610b2642b5a2eb9b4e55bbc40af2
ankursharma1930 <ankur.ank1230@gmail.com>
2023-02-13 14:17:02 +0530

    created supplier componenet and show supplier list

--- src/app/pages/supplier/supplier.component.ts
+++ src/app/pages/supplier/supplier.component.ts
@@ -1,4 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Subject, Subscription } from 'rxjs';
+
+
+
+const GET_SUPPLIERS = gql`
+  query suppliers {
+    suppliers {
+    id
+    name
+    code
+    country
+    currency
+    postcode
+  }
+  }
+`
+
+const GET_SUPPLIER = gql`
+  query user($id: ID!) {
+    supplier(id: $id) {
+    id
+    name
+    code
+    country
+    currency
+    postcode
+  }
+  }
+`
 
 @Component({
   selector: 'app-supplier',
@@ -6,10 +36,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./supplier.component.css']
 })
 export class SupplierComponent implements OnInit {
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject();
+  loading: boolean | undefined
+  suppliers: any
+  
+  
+  name!: string;
+  code!: string;
+  country!: string;
+  currency!: string;
+  postcode!: string;
 
-  constructor() { }
+  supplierId: any;
+  
+  error:any;
+
+  private querySubscription: Subscription | undefined
+ 
+
+  constructor(private apollo: Apollo) { }
+  deleteModal:any;
+  createModal:any;
+  updateModal:any;
+  idToDelete:number = 0;
 
   ngOnInit(): void {
+
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10
+    };
+
+    this.querySubscription = this.apollo
+      .watchQuery<any>({
+        query: GET_SUPPLIERS
+      })
+      .valueChanges.subscribe(({ data, loading }) => {
+        this.loading = loading
+        this.suppliers = data.suppliers
+        this.dtTrigger.next(null);
+      })
+
   }
 
 }
