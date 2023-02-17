4f5639c1f9ee759b17eeef94ff908ba335b23bb8
ankursharma1930 <ankur.ank1230@gmail.com>
2023-02-17 15:33:07 +0530

    add supplier data from api to local is working

--- src/app/pages/supplier/api-all-supplier/api-all-supplier.component.ts
+++ src/app/pages/supplier/api-all-supplier/api-all-supplier.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject, Subscription } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Apollo, gql } from 'apollo-angular';
+import { ThisReceiver } from '@angular/compiler';
+
+
+const UPSERT_SUPPLIER = gql`
+mutation upsertSupplier($id: ID!, $name: String, $country: String, $currency: String, $postcode: String, $code:String) {
+  upsertSupplier(id: $id, name: $name, country: $country, currency: $currency, postcode: $postcode, code:$code) {
+    name
+  }
+}
+`;
 
 
 const apiUrl = "https://api.promodata.com.au/suppliers/";
@@ -26,7 +37,20 @@ export class ApiAllSupplierComponent implements OnInit, OnDestroy {
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject();
 
-  constructor(private http: HttpClient) { }
+  name!: string;
+  code!: string;
+  country!: string;
+  currency!: string;
+  postcode!: string;
+  totalSupplier: number = 0;
+  increment:number = 0;
+  supplierId: any;
+  
+  error:any;
+  complete: any;
+  private querySubscription: Subscription | undefined
+
+  constructor(private http: HttpClient, private apollo: Apollo) { }
 
   ngOnInit(): void {
     this.supplierModal = new window.bootstrap.Modal(
@@ -34,7 +58,7 @@ export class ApiAllSupplierComponent implements OnInit, OnDestroy {
     );
     this.dtOptions = {
       pagingType: 'full_numbers',
-      pageLength: 10
+      pageLength: 14
     };
   }
 
@@ -57,7 +81,52 @@ export class ApiAllSupplierComponent implements OnInit, OnDestroy {
 
         }
       );
+  }
 
+  addInSystem(getData:any):void{
+    this.message = "Please wait we are sending data to your system...";
+    this.getclass = "warning";
+    this.totalSupplier = getData.length;
+    getData.forEach( (value: any) => {  
+      
+      
+      this.postcode = "NULL";
+      if(value.details.addresses != undefined && value.details.addresses[0] != undefined){
+        this.postcode = value.details.addresses[0].postcode;
+      }
+      
+      this.apollo
+      .mutate({
+        mutation: UPSERT_SUPPLIER,
+        variables: { 
+          id:value.id,
+          name:value.name,
+          country:String(value.country),
+          currency:"AUD",
+          code:value.details.appa_identifier,
+          postcode:this.postcode
+        }
+      })
+      .subscribe((result: any) => {
+        
+        if(result.data?.upsertSupplier){
+          console.log("Supplier updated"+ value.id);
+          this.increment++;
+          if(this.increment == this.totalSupplier){
+            this.message = "All Suppliers got imported and updated into your system, you can close the popup";
+            this.getclass = "success";
+          }
+        }
+        
+      },
+        error => {
+          this.message = "there was an error sending the query"+value.id;
+          this.getclass = "danger";
+        },
+        
+      );
+      
+    });  
 
   }
 
