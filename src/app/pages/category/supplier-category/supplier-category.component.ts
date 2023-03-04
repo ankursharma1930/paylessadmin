037bb8879f1e54a14a7814e120670db93b792e33
ankursharma1930 <ankur.ank1230@gmail.com>
2023-03-04 17:57:32 +0530

    supplier section

--- src/app/pages/category/supplier-category/supplier-category.component.ts
+++ src/app/pages/category/supplier-category/supplier-category.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Apollo, gql, Subscription } from 'apollo-angular';
+import {MessageService} from 'primeng/api';
+
 
 @Component({
   selector: 'app-supplier-category',
   templateUrl: './supplier-category.component.html',
   styleUrls: ['./supplier-category.component.css']
 })
-export class SupplierCategoryComponent implements OnInit {
-
+export class SupplierCategoryComponent implements OnChanges {
+  @Input() catId:any;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
   }
 
 }
