fdbb23418640ba4dce6a59965868e96477e6bacb
ankursharma1930 <ankur.ank1230@gmail.com>
2023-03-07 07:34:42 +0530

    updatec omponenet

--- src/app/pages/category/supplier-category/supplier-category.component.ts
+++ src/app/pages/category/supplier-category/supplier-category.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
-import {MessageService} from 'primeng/api';
+import { MessageService } from 'primeng/api';
 import { Subject, Subscription } from 'rxjs';
 
 
@@ -43,6 +43,14 @@ const GET_FILTER_SUPPLIERS = gql`
   }
 `;
 
+const CREATE_CATEGORY_SUPPLIER = gql`
+mutation createCategorySupplier($supplier_id: ID!, $category_id: ID! ) {
+  createCategorySupplier(supplier_id: $supplier_id, category_id: $category_id) {
+    id
+  }
+}
+`;
+
 declare var window: any;
 @Component({
   selector: 'app-supplier-category',
@@ -50,122 +58,169 @@ declare var window: any;
   styleUrls: ['./supplier-category.component.css']
 })
 export class SupplierCategoryComponent implements OnChanges, OnDestroy {
-  @Input() catId:any;
+  @Input() catId: any;
   supplierModal: any;
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject();
-  catName:string=''
-  supplier:any
-  supplierCompleteData:any;
-  savedSuppliers:Array<String> = [];
+  catName: string = ''
+  supplier: any
+  supplierCompleteData: any;
+  savedSuppliers: Array<String> = [];
 
   private querySubscription: Subscription | undefined
 
-  constructor(private apollo: Apollo,private messageService: MessageService) { }
+  constructor(private apollo: Apollo, private messageService: MessageService) { }
 
-  
+
   ngOnChanges(changes: SimpleChanges): void {
     this.savedSuppliers = [];
-    if(changes['catId'] && !changes['catId'].firstChange){
+    if (changes['catId'] && !changes['catId'].firstChange) {
       this.supplierModal = new window.bootstrap.Modal(
         document.getElementById('supplierModal')
       );
-      
+
       this.dtOptions = {
         pagingType: 'full_numbers',
         pageLength: 10
       };
-      
+
 
       //Fetch category name
       this.apollo
-       .watchQuery<any>({
-         query: GET_CATEGORY,
-         variables: {
-          id: Number(this.catId)
-         },
-       })
-       .valueChanges.subscribe(({ data, loading }) => {
-        
-        this.catName = data.category.name;
-       }
-       )
+        .watchQuery<any>({
+          query: GET_CATEGORY,
+          variables: {
+            id: Number(this.catId)
+          },
+        })
+        .valueChanges.subscribe(({ data, loading }) => {
+
+          this.catName = data.category.name;
+        }
+        )
 
 
 
 
       this.apollo
-       .watchQuery<any>({
-         query: GET_CATEGORY_SUPPLIER,
-         variables: {
-          category_id: Number(this.catId)
-         },
-         fetchPolicy: 'no-cache',
-         errorPolicy: 'ignore'
-       })
-       .valueChanges.subscribe(({ data, loading }) => {
-        if(data.category_supplier.length){
-          this.supplier = data.category_supplier;
-          for(var val of data.category_supplier){
-            this.savedSuppliers.push(val.supplier.id)
-            
+        .watchQuery<any>({
+          query: GET_CATEGORY_SUPPLIER,
+          variables: {
+            category_id: Number(this.catId)
+          },
+          fetchPolicy: 'no-cache',
+          errorPolicy: 'ignore'
+        })
+        .valueChanges.subscribe(({ data, loading }) => {
+          if (data.category_supplier.length) {
+            this.supplier = data.category_supplier;
+            for (var val of data.category_supplier) {
+              this.savedSuppliers.push(val.supplier.id)
+
+            }
           }
-      }
-       },
-       error => {
-         this.messageService.clear();
-         this.messageService.add({severity:'error',  detail:'Not Able to fetch the Filter data!'});
-       }
-       )
+        },
+          error => {
+            this.messageService.clear();
+            this.messageService.add({ severity: 'error', detail: 'Not Able to fetch the Filter data!' });
+          }
+        )
 
 
 
     }
+
+  }
+
+  //This function is for to fetch the all supplier inpopup
+
+  fetchSupplier() {
+
+
+    this.apollo
+      .watchQuery<any>({
+        query: GET_CATEGORY_SUPPLIER,
+        variables: {
+          category_id: Number(this.catId)
+        },
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'ignore'
+      })
+      .valueChanges.subscribe(({ data, loading }) => {
+        if (data.category_supplier.length) {
+          this.supplier = data.category_supplier;
+          for (var val of data.category_supplier) {
+            this.savedSuppliers.push(val.supplier.id)
+          }
+        }
+
+        if (this.savedSuppliers.length) {
+          this.querySubscription = this.apollo
+            .watchQuery<any>({
+              query: GET_FILTER_SUPPLIERS,
+              variables: {
+                excludedIds: this.savedSuppliers
+              },
+              fetchPolicy: 'no-cache',
+              errorPolicy: 'ignore'
+            })
+            .valueChanges.subscribe(({ data, loading }) => {
+              this.supplierCompleteData = data.fsuppliers;
+              this.dtTrigger.next(null);
+            }, error => {
+              this.messageService.add({ severity: 'error', detail: 'Not Able to fetch the Supplier data!' });
+            })
+    
+        } else {
+          this.querySubscription = this.apollo
+            .watchQuery<any>({
+              query: GET_FILTER_SUPPLIERS,
+              variables: {
+                excludedIds: this.savedSuppliers
+              },
+              fetchPolicy: 'no-cache',
+              errorPolicy: 'ignore'
+            })
+            .valueChanges.subscribe(({ data, loading }) => {
+    
+              this.supplierCompleteData = data.fsuppliers;
+              this.dtTrigger.next(null);
+            }, error => {
+              this.messageService.add({ severity: 'error', detail: 'Not Able to fetch the Supplier data!' });
+            })
+        }
+        this.supplierModal.show();
+      },
+        error => {
+          this.messageService.clear();
+          this.messageService.add({ severity: 'error', detail: 'Not Able to fetch the Filter data!' });
+        }
+      )
     
   }
 
-  fetchSupplier(){
-    this.supplierModal.show();
-    console.log(this.savedSuppliers);
-    if(this.savedSuppliers.length){
+  //This function is to create a new supplier for tab
 
-      this.querySubscription = this.apollo
-      .watchQuery<any>({
-        query: GET_FILTER_SUPPLIERS,
+  addSupplier(event: any, id: any) {
+    this.apollo
+      .mutate({
+        mutation: CREATE_CATEGORY_SUPPLIER,
         variables: {
-          excludedIds: this.savedSuppliers
-         },
-         fetchPolicy: 'no-cache',
-         errorPolicy: 'ignore'
+          supplier_id: id,
+          category_id: this.catId
+        }
       })
-      .valueChanges.subscribe(({ data, loading }) => {
-        this.supplierCompleteData = data.fsuppliers;
-        this.dtTrigger.next(null);
-      }, error=>{
-        this.messageService.add({severity:'error',  detail:'Not Able to fetch the Supplier data!'});
-      })
-
-    }else{
-      this.querySubscription = this.apollo
-      .watchQuery<any>({
-        query: GET_FILTER_SUPPLIERS,
-        variables: {
-          excludedIds: this.savedSuppliers
-         },
-         fetchPolicy: 'no-cache',
-         errorPolicy: 'ignore'
-      })
-      .valueChanges.subscribe(({ data, loading }) => {
-        
-        this.supplierCompleteData = data.fsuppliers;
-        this.dtTrigger.next(null);
-      }, error=>{
-        this.messageService.add({severity:'error',  detail:'Not Able to fetch the Supplier data!'});
-      })
-    }
-    
+      .subscribe((result: any) => {
+        this.messageService.clear();
+        if (result.data?.createCategorySupplier) {
+          this.messageService.add({ severity: 'success', summary: 'yahooo!', detail: 'Supplier Added' });
+        }
+      },
+        error => {
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Somthing is wrong with supplier', sticky: true });
+        }
+      );
   }
-
   ngOnDestroy(): void {
     this.dtTrigger.unsubscribe();
   }
