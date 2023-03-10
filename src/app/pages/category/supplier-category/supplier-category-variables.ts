87f6b75b3a7b251e94b62aa22c6abcd120110925
ankursharma1930 <ankur.ank1230@gmail.com>
2023-03-10 09:30:42 +0530

    markeup quantity break is working

--- src/app/pages/category/supplier-category/supplier-category-variables.ts
+++ src/app/pages/category/supplier-category/supplier-category-variables.ts
@@ -61,19 +61,53 @@ query fsuppliers($excludedIds: [ID!]) {
 `;
 
 export const CREATE_CATEGORY_SUPPLIER = gql`
-mutation createCategorySupplier($supplier_id: ID!, $category_id: ID! ) {
-  createCategorySupplier(supplier_id: $supplier_id, category_id: $category_id) {
+mutation createCategorySupplier($supplier_id: ID!, $category_id: ID!, $mark_up_by_quantity: String) {
+  createCategorySupplier(supplier_id: $supplier_id, category_id: $category_id, mark_up_by_quantity:$mark_up_by_quantity) {
     id
   }
 }
 `;
 
 export const UPDATE_CATEGORY_SUPPLIER = gql`
-mutation updateCategorySupplier($id:ID!, $category_id: ID!, $lead_time:String ) {
-  updateCategorySupplier(id: $id, category_id: $category_id, lead_time:$lead_time) {
+mutation updateCategorySupplier($id:ID!, $category_id: ID!, $lead_time:String, $default_mark_up:String,$mark_up_by_quantity:String ) {
+  updateCategorySupplier(id: $id, category_id: $category_id, lead_time:$lead_time, default_mark_up:$default_mark_up, mark_up_by_quantity:$mark_up_by_quantity) {
     id
     category_id
     lead_time
+    default_mark_up
+    mark_up_by_quantity
   }
 }
 `;
+
+export const DELETE_CATEGORY_SUPPLIER = gql`
+mutation deleteCategorySupplier($id: ID!) {
+  deleteCategorySupplier(id: $id) {
+    id
+  }
+}
+`;
+
+interface Qty{
+  qty?:string
+  realvalue?:string
+}
+
+export const QuantitySlab: Qty[] = [
+  {qty:"1" },
+  {qty:"5" },
+  {qty:"12" },
+  {qty:"25" },
+  {qty:"50" },
+  {qty:"100" },
+  {qty:"250" },
+  {qty:"500" },
+  {qty:"1000" },
+  {qty:"2500" },
+  {qty:"5000" },
+  {qty:"10000" },
+  {qty:"25000" },
+  {qty:"50000" },
+  {qty:"100000" },
+  {qty:"250000" },{qty:"500000" },{qty:"1000000" },
+]
--- src/app/pages/category/supplier-category/supplier-category.component.ts
+++ src/app/pages/category/supplier-category/supplier-category.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges, OnDestroy, } from '@angular/core';
-import {  FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
 import { Apollo } from 'apollo-angular';
 import { MessageService } from 'primeng/api';
 import { first, Subject, Subscription } from 'rxjs';
-import { GET_CATEGORY_SUPPLIER,GET_CATEGORY_SUPPLIER_BYID, GET_CATEGORY, GET_FILTER_SUPPLIERS, CREATE_CATEGORY_SUPPLIER, UPDATE_CATEGORY_SUPPLIER } from './supplier-category-variables';
+import { QuantitySlab, DELETE_CATEGORY_SUPPLIER, GET_CATEGORY_SUPPLIER, GET_CATEGORY_SUPPLIER_BYID, GET_CATEGORY, GET_FILTER_SUPPLIERS, CREATE_CATEGORY_SUPPLIER, UPDATE_CATEGORY_SUPPLIER } from './supplier-category-variables';
 
 
 interface Leadtime {
   first?: string,
-  second?:string,
-  third?:string,
-  forth?:string
+  second?: string,
+  third?: string,
+  forth?: string
 }
+
 
 declare var window: any;
 @Component({
@@ -28,17 +29,21 @@ export class SupplierCategoryComponent implements OnChanges, OnDestroy {
   supplier: any
   supplierCompleteData: any;
   savedSuppliers: Array<String> = [];
-  clickedSupplierId:string=""
+  clickedSupplierId: string = ""
   lead_time: Leadtime = {};
-  first_lead:string=''
-  sec_lead:string=''
-  third_lead:string=''
-  forth_lead:string=''
- 
+  first_lead: string = ''
+  sec_lead: string = ''
+  third_lead: string = ''
+  forth_lead: string = ''
+  printA_Z: Array<string> = []
+  default_mark_up: string = ''
+  //mark_up_by_quantity: string[] = [];
+  quantity_slab: readonly any[] = QuantitySlab
+  
 
   private querySubscription: Subscription | undefined
 
-  constructor(private apollo: Apollo, private messageService: MessageService,private fb: FormBuilder) { }
+  constructor(private apollo: Apollo, private messageService: MessageService, private fb: FormBuilder) { }
 
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -69,7 +74,7 @@ export class SupplierCategoryComponent implements OnChanges, OnDestroy {
       //Fetch category supplier 
       this.getSupplier();
     }
-
+    this.printAZ();
   }
 
   //This function is for to fetch the all supplier inpopup
@@ -108,7 +113,7 @@ export class SupplierCategoryComponent implements OnChanges, OnDestroy {
             }, error => {
               this.messageService.add({ severity: 'error', detail: 'Not Able to fetch the Supplier data!' });
             })
-    
+
         } else {
           this.querySubscription = this.apollo
             .watchQuery<any>({
@@ -120,7 +125,7 @@ export class SupplierCategoryComponent implements OnChanges, OnDestroy {
               errorPolicy: 'ignore'
             })
             .valueChanges.subscribe(({ data, loading }) => {
-    
+
               this.supplierCompleteData = data.fsuppliers;
               this.dtTrigger.next(null);
             }, error => {
@@ -134,7 +139,7 @@ export class SupplierCategoryComponent implements OnChanges, OnDestroy {
           this.messageService.add({ severity: 'error', detail: 'Not Able to fetch the Filter data!' });
         }
       )
-    
+
   }
 
   //This function is to create a new supplier from tab
@@ -145,8 +150,10 @@ export class SupplierCategoryComponent implements OnChanges, OnDestroy {
         mutation: CREATE_CATEGORY_SUPPLIER,
         variables: {
           supplier_id: id,
-          category_id: this.catId
+          category_id: this.catId,
+          mark_up_by_quantity:JSON.stringify(QuantitySlab)
         }
+        
       })
       .subscribe((result: any) => {
         this.messageService.clear();
@@ -154,6 +161,7 @@ export class SupplierCategoryComponent implements OnChanges, OnDestroy {
           this.messageService.add({ severity: 'success', summary: 'yahooo!', detail: 'Supplier Added' });
 
           this.getSupplier();
+          console.log(QuantitySlab);
         }
       },
         error => {
@@ -163,29 +171,31 @@ export class SupplierCategoryComponent implements OnChanges, OnDestroy {
   }
 
 
-  getSupplier(){
+  getSupplier() {
     this.apollo
-          .watchQuery<any>({
-            query: GET_CATEGORY_SUPPLIER,
-            variables: {
-              category_id: Number(this.catId)
-            },
-            fetchPolicy: 'no-cache',
-            errorPolicy: 'ignore'
-          })
-          .valueChanges.subscribe(({ data, loading }) => {
-            if (data.category_supplier.length) {
-              this.supplier = data.category_supplier;
-              for (var val of data.category_supplier) {
-                this.savedSuppliers.push(val.supplier.id)
-              }
-            }
-          },
-            error => {
-              this.messageService.clear();
-              this.messageService.add({ severity: 'error', detail: 'Not Able to fetch the Filter data!' });
-            }
-          )
+      .watchQuery<any>({
+        query: GET_CATEGORY_SUPPLIER,
+        variables: {
+          category_id: Number(this.catId)
+        },
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'ignore'
+      })
+      .valueChanges.subscribe(({ data, loading }) => {
+        if (data.category_supplier.length) {
+          this.supplier = data.category_supplier;
+          for (var val of data.category_supplier) {
+            this.savedSuppliers.push(val.supplier.id)
+          }
+        }else{
+          this.supplier = null
+        }
+      },
+        error => {
+          this.messageService.clear();
+          this.messageService.add({ severity: 'error', detail: 'Not Able to fetch the Filter data!' });
+        }
+      )
   }
 
 
@@ -194,31 +204,33 @@ export class SupplierCategoryComponent implements OnChanges, OnDestroy {
   }
 
 
-  onSubmit(f:NgForm){
+  onSubmit(f: NgForm) {
+    // console.log(typeof JSON.stringify(this.quantity_slab));
+    this.lead_time = { first: this.first_lead, second: this.sec_lead, third: this.third_lead, forth: this.forth_lead }
     
-    this.lead_time = {first:this.first_lead,second:this.sec_lead,third:this.third_lead,forth:this.forth_lead}
     
     this.apollo
       .mutate({
         mutation: UPDATE_CATEGORY_SUPPLIER,
         variables: {
-          id:this.clickedSupplierId,
-          category_id:this.catId,
-          lead_time:JSON.stringify(this.lead_time)
-          
+          id: this.clickedSupplierId,
+          category_id: this.catId,
+          lead_time: JSON.stringify(this.lead_time),
+          default_mark_up: this.default_mark_up,
+          mark_up_by_quantity:JSON.stringify(this.quantity_slab)
         }
       })
       .subscribe((result: any) => {
-        
-        if(result.data?.updateCategorySupplier){
+
+        if (result.data?.updateCategorySupplier) {
           this.messageService.clear();
-          this.messageService.add({severity:'success',  detail:'Category Supplier Updated!'});
-          
+          this.messageService.add({ severity: 'success', detail: 'Category Supplier Updated!' });
+          QuantitySlab.forEach(obj => delete obj.realvalue);
         }
       },
         error => {
           this.messageService.clear();
-          this.messageService.add({severity:'error',  detail:'Something is wrong, please refresh the page and try again'});
+          this.messageService.add({ severity: 'error', detail: 'Something is wrong, please refresh the page and try again' });
         }
       );
 
@@ -226,30 +238,68 @@ export class SupplierCategoryComponent implements OnChanges, OnDestroy {
 
   //Fetch category supplier using id, when select the particular supplier in accordin
 
-  fetchCategorySupplierData(categorySupplierId:any){
+  fetchCategorySupplierData(categorySupplierId: any) {
     this.clickedSupplierId = categorySupplierId;
     this.first_lead = this.sec_lead = this.third_lead = this.forth_lead = "";
+    this.quantity_slab = QuantitySlab
+    console.log(QuantitySlab)
     this.apollo
       .watchQuery<any>({
         query: GET_CATEGORY_SUPPLIER_BYID,
         variables: {
           id: categorySupplierId
-        }
-        
+        },
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'ignore'
       })
       .valueChanges.subscribe(({ data, loading }) => {
         if (data.particular_category_supplier) {
-          if(data.particular_category_supplier.lead_time){
+          if (data.particular_category_supplier.lead_time) {
             this.lead_time = JSON.parse(data.particular_category_supplier.lead_time);
-            this.first_lead = this.lead_time.first?this.lead_time.first:''
-            this.sec_lead = this.lead_time.second?this.lead_time.second:''
-            this.third_lead = this.lead_time.third?this.lead_time.third:''
-            this.forth_lead = this.lead_time.forth?this.lead_time.forth:''
-        }
+            this.first_lead = this.lead_time.first ? this.lead_time.first : ''
+            this.sec_lead = this.lead_time.second ? this.lead_time.second : ''
+            this.third_lead = this.lead_time.third ? this.lead_time.third : ''
+            this.forth_lead = this.lead_time.forth ? this.lead_time.forth : ''
+          }
+          this.default_mark_up = data.particular_category_supplier.default_mark_up ? data.particular_category_supplier.default_mark_up : "";
+          this.quantity_slab = data.particular_category_supplier.mark_up_by_quantity?JSON.parse(data.particular_category_supplier.mark_up_by_quantity):[]
         }
       }
       )
   }
 
+  removeSupplier(id:any){
+    if(confirm("Are you sure?")) {
+      this.apollo
+      .mutate({
+        mutation: DELETE_CATEGORY_SUPPLIER,
+        variables: {
+          id: id
+        }
+      })
+      .subscribe(
+        ({ data }) => {
+          this.getSupplier();
+          this.messageService.add({ severity: 'success', detail: 'Supplier deleted' });
+        },
+        error => {
+          this.messageService.add({ severity: 'error', detail: 'Something is wrong!' });
+        }
+      )
+    }
+    
+  }
 
+  //type A-z 
+  printAZ(): any {
+    let i;
+    for (i = 65; i <= 90; i++) {
+      this.printA_Z.push(String.fromCharCode(i))
+    }
+  }
+
+  //onCHange markup level
+  onChangeMarkup(){
+    
+  }
 }
